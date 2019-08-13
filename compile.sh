f1335dfb3b8f1daacde6d3f27db65707c39cb30f
Phillip Stephens <antidote.crk@gmail.com>
2019-08-13 16:48:40 -0700

    Make the build system a little smarter by determining GCC version at launch, and give `compile.sh` an actual result

--- compile.sh
+++ compile.sh
@@ -2,6 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 python3 ./PrimeAPI/script/BuildModule.py . default.dol -v
-cp -v default_mod.dol ../mp1/files/default.dol
-cp -v default_mod.dol ../mp1/sys/main.dol
-cp -v build/Mod.rel ../mp1/files/Mod.rel
+ret=$?
+if [ $ret -eq 0 ]; then
+  cp -v default_mod.dol ../mp1/files/default.dol
+  cp -v default_mod.dol ../mp1/sys/main.dol
+  cp -v build/Mod.rel ../mp1/files/Mod.rel
+else
+  echo Failed to compile module
+fi
